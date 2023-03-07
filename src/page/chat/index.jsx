2beaff63310d586fbff658d4db74135e4a691799
TREXA23187 <498111557@qq.com>
2023-03-07 20:19:47 +0000

    feat: add base64 picture

--- src/page/chat/index.jsx
+++ src/page/chat/index.jsx
@@ -1,6 +1,28 @@
 import React, { useEffect, useState } from 'react';
+import { Image } from 'antd';
+import { timeDist } from './img/img_path';
+import { html } from './data/chat.js';
 
 export default function Chat(props) {
   const { visible } = props;
-  return <div style={{ visibility: visible ? 'visible' : 'hidden' }}>Chat</div>;
+  return (
+    <div style={{ visibility: visible ? 'visible' : 'hidden' }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {/* <Image
+          width={200}
+          // height={615}
+          src={timeDist}
+        /> */}
+        return (
+        <iframe
+          title='resg'
+          srcDoc={html}
+          style={{ width: '100%', border: '0px', height: '1100px' }}
+          sandbox='allow-same-origin allow-scripts allow-popups allow-forms'
+          scrolling='auto'
+        />
+        );
+      </div>
+    </div>
+  );
 }
